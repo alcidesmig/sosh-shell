0ef934411c3cc323bb7dc74815e2036df52dbe34
Alcides Mignoso e Silva <alcidesmig@gmail.com>
2019-09-26 11:42:02 -0300

    Arrumar: signals

--- shell.cpp
+++ shell.cpp
@@ -80,7 +80,8 @@ void handle(char *result)
 
     int background = result[strlen(result) - 1] == '&';
 
-    if(background) {
+    if(background)
+    {
         result[strlen(result) - 1] = '\0';
     }
 
@@ -225,14 +226,60 @@ void handle(char *result)
 }
 
 
+
+void sigHandler(int sig)
+{
+    printf("%d\n", sig);
+}
 /* Make sure the shell is running interactively as the foreground job
    before proceeding. */
 
+
+pid_t shell_pgid;
+struct termios shell_tmodes;
+int shell_terminal;
+int shell_is_interactive;
 void
 init_shell ()
 {
     stop_shell = 0;
     cout << "Shell iniciado" << endl;
+
+    /* See if we are running interactively.  */
+    shell_terminal = STDIN_FILENO;
+    shell_is_interactive = isatty (shell_terminal);
+
+    if (shell_is_interactive)
+    {
+        /* Loop until we are in the foreground.  */
+        while (tcgetpgrp (shell_terminal) != (shell_pgid = getpgrp ()))
+            kill (- shell_pgid, SIGTTIN);
+
+        /* Ignore interactive and job-control signals.  */
+        signal (SIGINT, sigHandler);
+        signal (SIGQUIT, sigHandler);
+        signal (SIGTSTP, sigHandler);
+        signal (SIGTTIN, sigHandler);
+        signal (SIGTTOU, sigHandler);
+        signal (SIGCHLD, sigHandler);
+
+        /* Put ourselves in our own process group.  */
+        shell_pgid = getpid ();
+        if (setpgid (shell_pgid, shell_pgid) < 0)
+        {
+            perror ("Couldn't put the shell in its own process group");
+            exit (1);
+        }
+
+        /* Grab control of the terminal.  */
+        tcsetpgrp (shell_terminal, shell_pgid);
+
+        /* Save default terminal attributes for shell.  */
+        tcgetattr (shell_terminal, &shell_tmodes);
+    }
+
+
+
 
     /* Linenoise functions */
 
