84d6568c7c9899f29bc4253c6e9237a65ba3c249
Alcides Mignoso <alcidesmig@gmail.com>
2019-10-03 20:54:24 -0300

    Update defines.h

--- defines.h
+++ defines.h
@@ -6,31 +6,4 @@
 
 #define HISTORY "/tmp/shell_so_history"
 
-
-/* A process is a single process.  */
-typedef struct process
-{
-  struct process *next;       /* next process in pipeline */
-  char **argv;                /* for exec */
-  pid_t pid;                  /* process ID */
-  char completed;             /* true if process has completed */
-  char stopped;               /* true if process has stopped */
-  int status;                 /* reported status value */
-} process;
-
-/* A job is a pipeline of processes.  */
-typedef struct job
-{
-  struct job *next;           /* next active job */
-  char *command;              /* command line, used for messages */
-  process *first_process;     /* list of processes in this job */
-  pid_t pgid;                 /* process group ID */
-  char notified;              /* true if user told about stopped job */
-  struct termios tmodes;      /* saved terminal modes */
-  int stdin, stdout, stderr;  /* standard i/o channels */
-} job;
-
-/* The active jobs are linked into a list.  This is its head.   */
-extern job *first_job;
-
 #endif /* DEFINES_H */
